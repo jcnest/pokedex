6c83dc4d3249cbf6e5a0f1406df15743a78a871b
José Carlos Palma <palmahn@gmail.com>
2022-10-15 19:33:52 -0600

    feat(seed): Implementación inicial.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from '@common/common.module';
 import { PokemonModule } from '@pokemon/pokemon.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
     imports: [
@@ -13,6 +14,7 @@ import { PokemonModule } from '@pokemon/pokemon.module';
         MongooseModule.forRoot('mongodb://localhost:27018/pokedex'),
         CommonModule,
         PokemonModule,
+        SeedModule,
     ],
 })
 export class AppModule {}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+    private readonly axios: AxiosInstance = axios;
+
+    async executeSeed() {
+        const { data } = await this.axios.get<PokeResponse>(
+            'https://pokeapi.co/api/v2/pokemon?limit=10',
+        );
+
+        const pokemons = data.results.map(({ name, url }) => {
+            const no = +url.split('/').at(-2);
+            return { name, no };
+        });
+
+        // return 'Seed successfully executed';
+        return pokemons;
+    }
+}
