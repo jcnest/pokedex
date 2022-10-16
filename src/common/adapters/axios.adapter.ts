b25fb5356a81c608536243babd08f099baf88b66
José Carlos Palma <palmahn@gmail.com>
2022-10-15 20:33:18 -0600

    feat(common): Adaptadores para peticiones http.

---
+++ src/common/adapters/axios.adapter.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { HttpAdapter } from '@common/interfaces';
+
+@Injectable()
+export class AxiosAdapter implements HttpAdapter {
+    private readonly axios: AxiosInstance = axios;
+
+    async get<T>(url: string): Promise<T> {
+        try {
+            const { data } = await this.axios.get<T>(url);
+            return data;
+        } catch (err) {
+            throw new Error(`Internal error - Check logs`);
+        }
+    }
+}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Pokemon } from '@pokemon/entities';
+import { AxiosAdapter } from '@common/adapters';
 import { PokeResponse } from './interfaces';
 
 @Injectable()
 export class SeedService {
-    private readonly axios: AxiosInstance = axios;
-
     constructor(
         @InjectModel(Pokemon.name)
         private readonly pokemonModel: Model<Pokemon>,
+        private readonly http: AxiosAdapter,
     ) {}
 
     async executeSeed() {
-        const { data } = await this.axios.get<PokeResponse>(
+        const data = await this.http.get<PokeResponse>(
             'https://pokeapi.co/api/v2/pokemon?limit=512',
         );
 
